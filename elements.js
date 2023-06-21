3ea7228c7474e864baac79cbefc2ef0fc31f968a
Fábio_Luiz <fabiolljunior23@gmail.com>
2023-06-21 14:08:18 -0300

    Create elements.js

---
+++ elements.js
@@ -0,0 +1,19 @@
+const buttonStart = document.querySelector('.start')
+const buttonPause = document.querySelector('.pause')
+const buttonSet = document.querySelector('.set')
+const buttonStop = document.querySelector('.stop')
+const buttonSoundOn = document.querySelector('.soundOn')
+const buttonSoundOff = document.querySelector('.soundOff')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+export const elements = {
+    buttonStart,
+    buttonPause,
+    buttonSet,
+    buttonStop,
+    buttonSoundOn,
+    buttonSoundOff,
+    minutesDisplay,
+    secondsDisplay
+}
